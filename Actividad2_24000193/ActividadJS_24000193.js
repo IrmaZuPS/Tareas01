708bc2dd82c2e16406425a829fdb7cac73e476f7
Irma Z. Piche S. <24000193@galileo.edu>
2024-02-05 00:02:56 -0600

    first commit

---
+++ Actividad2_24000193/ActividadJS_24000193.js
@@ -0,0 +1,51 @@
+var mes = prompt ("Ingrese su mes de nacimiento en números del 1 al 12")
+var dia = prompt ("Ingrese su día de nacimiento en números")
+
+if (mes ==4 && dia <= 20){
+  alert ("su signo es Aries");
+}else if(mes == 4 && dia >=21){
+  alert ("su signo es Tauro");
+}else if (mes == 5 && dia <=20){
+  alert ("su signo es Tauro");
+} else if (mes == 5 && dia >= 21){
+  alert ("su signo es Géminis");
+}else if (mes == 6 && dia <=20 ){
+  alert ("su signo es Géminis");
+}else if (mes == 6 && dia >=21 ){
+  alert ("su signo es Cáncer");
+}else if (mes == 7 && dia <=22 ){
+  alert ("su signo es Cáncer");
+}else if (mes == 7 && dia >=23 ){
+  alert ("su signo es Leo");
+}else if (mes == 8 && dia <=23 ){
+  alert ("su signo es Leo");
+}else if (mes == 8 && dia >=24 ){
+  alert ("su signo es Virgo");
+}else if (mes == 9 && dia <=22 ){
+  alert ("su signo es Virgo");
+}else if (mes == 9 && dia >=23 ){
+  alert ("su signo es Libra");
+}else if (mes == 10 && dia <=23 ){
+  alert ("su signo es Libra");
+}else if (mes == 10 && dia >=24 ){
+  alert ("su signo es Escorpio");
+}else if (mes == 11 && dia <=22 ){
+  alert ("su signo es Escorpio");
+}else if (mes == 11 && dia >=23 ){
+  alert ("su signo es Sagitario");
+}else if (mes == 12 && dia <=21 ){
+  alert ("su signo es Sagitario");
+}else if (mes == 12 && dia >=22 ){
+  alert ("su signo es Capricornio");
+}else if (mes == 1 && dia <=20 ){
+  alert ("su signo es Capricornio");
+}else if (mes == 1 && dia >=21 ){
+  alert ("su signo es Acuario");
+}else if (mes == 2 && dia <=19 ){
+  alert ("su signo es Acuario");
+}else if (mes == 2 && dia >=20 ){
+  alert ("su signo es Piscis");
+}else if (mes == 3 && dia <=20 ){
+  alert ("su signo es Piscis");
+}else if (mes == 3 && dia >=21 ){
+  alert ("su signo es Aries");}
